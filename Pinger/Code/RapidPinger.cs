cc47b80dffe58cdf1dc6bf2aa31f530853a41ddd
yaron <yaronthurm@gmail.com>
2015-03-20 14:10:58 +0200

    adding new class RapidPinger
    The class is just a skeleton for now

---
+++ Pinger/Code/RapidPinger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace PingTester
+{
+    public class RapidPingerEventArgs : EventArgs
+    {
+        public bool Success { get; private set;}
+    }
+
+    public class RapidPinger
+    {
+        public event ReplyRecievedHandler ReplyRecieved;     
+    }
+}
